de20ee47c425ae47e11747b23a7da2786b86b4f2
Sattorov Kamronbek <pubggameruzb.sila@gmail.com>
2023-10-10 18:45:03 +0500

    the end

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-// O'ZIMIZNING USUL
-import styles from "./App.module.css";
-import buttonStyle from './Button.module.css'
-import Button from "./Button.js";
-// ENG YOMON USULLI REEACTDAGI CSS
-
-// const styles = {
-//   color: "red",
-//   fontSize: "16px", // not font-size: 16px;
-// };
+import "./App.css";
+import { TodoWrapper } from "./Components/TodoWrapper";
 
 function App() {
   return (
-      <div className={styles.container}>
-        <Button className={buttonStyle.btn2}>Github</Button>
-        <Button className={buttonStyle.btn1}>acc</Button>
-        <Button>sotiladi!!!</Button>
-
-      </div>
+    <div className="App">
+      <TodoWrapper />
+    </div>
   );
 }
 
--- src/Button.js
+++
@@ -1,5 +0,0 @@
-import styles from './Button.module.css'
-const Button = (props) => {
-    return <button className=''>{props.children}</button>
-}
-export default Button
